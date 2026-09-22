2ec80143f4d53a2f70bb7611bec2328d5fef1b93
Alişer <sezoysr12@gmail.com>
2026-09-22 03:12:30 +0300

    Kotlin ve Java JVM hedeflerini 17 yap

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,13 @@ android {
         versionName = "0.1.0"
     }
     buildFeatures { compose = true }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
